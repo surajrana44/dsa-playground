d6c23a0eab3b5e870b4978b379df99505435a15d
Suraj Singh Rana <surajsinghrana061@gmail.com>
2025-10-05 14:09:47 +0530

    storing 2d array in dynamic memory

---
+++ memory_allocation/dynamic_2d_array.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+int main(){
+/*
+int n;
+cin>>n;
+ 
+// creation a 2d array
+int ** arr = new int*[n];
+for(int i=0;i<n;i++){
+    arr[i]=new int [n];
+
+}
+
+// taking input 
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        cin >> arr[i][j];
+    }
+}
+// taking output
+cout<<endl;
+
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        cout<< arr[i][j];
+    } cout<<endl;
+}
+*/
+
+int row;  // crating 3*4 matrix
+cin>>row;
+
+int col;
+cin>>col;
+
+// creating 2d  array
+int **arr = new int*[row];
+for(int i=0;i<row;i++){
+   
+    arr[i] = new int[col];
+}
+
+
+// taking input
+
+for (int i=0; i<row ;i++){
+    for(int j=0; j<col ;j++){
+        cin >> arr[i][j];
+        
+    }
+}
+
+// taking ouotput 
+cout<<endl;
+
+for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+        cout<< arr[i][j];
+    } cout<<endl;
+}
+
+// releasing memory 
+for(int i=0; i<row ; i++){
+    delete []arr[i];
+
+}
+
+delete []arr;
+
+
+// in this we create :
+// how to crate 2d array dynamically 
+// how to take input and give output
+// delete the used heap memory sequence wise  
+
+
+
+
+}
